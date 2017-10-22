88ac8ee5fe6989d4c26280fce1ef5f6bb48a233a
Freddie Lichtenstein <fredlich@fredlich.local>
2017-10-22 21:17:34 +0100

    german spellings

--- app/getpremierleaguescores.py
+++ app/getpremierleaguescores.py
@@ -203,11 +203,12 @@ def getGoalLinksFromReddit(array_of_teams_goalscorers):
 
 	for scorer in away_goalscorers:
 		if 'oe' in scorer:
-			scorer = scorer.replace('oe','e')
+			scorer = scorer.replace('oe','o')
 
 
 		if 'ue' in scorer:
-			scorer = scorer.replace('ue','e')
+			scorer = scorer.replace('ue','u')
+
 		posts = reddit.subreddit('soccer').search( scorer.split()[-1] + ' ' + home_team.split()[0], sort='new', time_filter='month')
 
 		
